2be5d3a92260371f9a5f38dad2a4801367dcc171
Gazi Akter <gaziakterdev@gmail.com>
2024-01-17 19:04:20 +0600

    Creating custom menu page

--- wp-crud-plugin.php
+++ wp-crud-plugin.php
@@ -14,7 +14,7 @@ Domain Path: /languages/
 /** 
  * Create Table 
  */
-function wp_cruud_activation(){
+function wp_crud_activation(){
     global $wpdb;
     $table_name = $wpdb->prefix . 'crud_data';
     $charset_collate = $wpdb->get_charset_collate();
@@ -32,4 +32,38 @@ function wp_cruud_activation(){
     dbDelta($sql);
 
 }
-register_activation_hook( __FILE__, "wp_cruud_activation");
+register_activation_hook( __FILE__, "wp_crud_activation");
+
+
+/**
+ * Creating custom admin menu item
+ */
+function custom_data_menu() {
+    $page_title = 'Custom Data';
+    $menu_title = 'Custom Data';
+    $capability = 'manage_options';
+    $menu_slug = 'custom-data';
+    $function = 'custom_data_page';
+    $icon_url = 'dashicons-admin-generic';
+    $position = 25;
+
+    add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position);
+
+    // Submenu pages
+    add_submenu_page($menu_slug, 'Add New', 'Add New', $capability, 'custom-data-add', 'custom_data_add_page');
+    add_submenu_page($menu_slug, 'Edit', 'Edit', $capability, 'custom-data-edit', 'custom_data_edit_page');
+}
+add_action('admin_menu', 'custom_data_menu');
+
+
+function custom_data_page() {
+    // Main custom data management page content
+}
+
+function custom_data_add_page() {
+    // Add new custom data page content
+}
+
+function custom_data_edit_page() {
+    // Edit custom data page content
+}
